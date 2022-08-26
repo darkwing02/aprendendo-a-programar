ee1feb66d9b6144cadac6a0732ff7fdaedbf9798
darkwing02 <motahunaldo24@gmail.com>
2022-08-26 19:49:01 -0300

    pratica

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,10 @@
+let numero = 123
+//saber se é par ou impar
+// divide por 2 se o resto for zero é par se n é impar
+if(numero % 2 == 0) {
+    console.log(`${numero} é par`)
+}  else {
+    console.log(`${numero} é ímpar`)
+}
+
+console.log(`${numero} é par`)
