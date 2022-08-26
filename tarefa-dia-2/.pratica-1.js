c3df35458b1a74ae6fdd5bd05e4424d74a3f66d9
darkwing02 <motahunaldo24@gmail.com>
2022-08-26 17:30:35 -0300

    pratica-1

---
+++ tarefa-dia-2/.pratica-1.js
@@ -0,0 +1,11 @@
+let nome = "hunaldo"
+let email = "motahunaldo24@gmail.com"
+let prifissao = "estudante"
+let idade = 17
+
+if(idade>= 18) {
+    console.log("pode jogar")
+} else {
+    console.log("não pode jogar")
+
+}
